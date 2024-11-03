330140dbf9b8a65023515d84df6134f0dff4a8ea
Lalit Jha <anandiit2006@gmail.com>
2024-11-03 11:18:56 -0700

    Added new code file

---
+++ code.py
@@ -0,0 +1,19 @@
+import torch
+import argparse
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+from datetime import datetime
+import gymnasium as gym
+import os, shutil
+import argparse
+
+
+def build_net(layer_shape, hidden_activation, output_activation):
+	'''Build net with for loop'''
+	layers = []
+	for j in range(len(layer_shape)-1):
+		act = hidden_activation if j < len(layer_shape)-2 else output_activation
+		layers += [nn.Linear(layer_shape[j], layer_shape[j+1]), act()]
+	return nn.Sequential(*layers)
